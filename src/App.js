9c401cff7c96f4d9b00cececc2b91ef43a22fe95
dorcasndungu <ndungumwihaki017@gmail.com>
2024-01-13 23:28:32 +0300

    sets up login form

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import AuthenticationForm from './components/AuthenticationForm';  
+import './App.css';
+
+function App() {
+  return (
+    <div >
+     
+   
+<h1>PM ALLOCATION</h1>
+<AuthenticationForm />
+</div>
+  );
+}
+
+export default App;
---
+++ src/components/AuthenticationForm.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import '../AuthenticationForm.css'; // Import the CSS file
+
+const AuthenticationForm = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your authentication logic here
+    console.log('Form submitted with:', formData);
+    // Example: Call authentication API or perform authentication logic
+  };
+
+  return (
+    <div>
+      <h2>Authentication Form</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange};/
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AuthenticationForm;
